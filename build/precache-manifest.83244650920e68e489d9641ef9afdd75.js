177e5b9e0330251a1ef63cce1c82c6e101da5cf9
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-11-20 13:12:03 +0200

    Initiate app.

---
+++ build/precache-manifest.83244650920e68e489d9641ef9afdd75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99595200f636bcc5774e",
+    "url": "/static/js/main.99595200.chunk.js"
+  },
+  {
+    "revision": "5ecabb3b7dda72e2d6c4",
+    "url": "/static/js/1.5ecabb3b.chunk.js"
+  },
+  {
+    "revision": "99595200f636bcc5774e",
+    "url": "/static/css/main.c5fb097b.chunk.css"
+  },
+  {
+    "revision": "9cbd01b61297cef4f67af41f1a4f47be",
+    "url": "/index.html"
+  }
+];
